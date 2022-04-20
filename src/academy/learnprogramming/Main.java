b7f7d8cf1312b9198ab270b6694ce546ee60162f
Sadid369 <sadid.jones@gmail.com>
2022-04-21 00:54:25 +0600

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char switchValue= 'E';
+    switch (switchValue){
+        case 'A':
+            System.out.println("value is "+switchValue);
+            break;
+        case  'B':
+            System.out.println("value is "+switchValue);
+            break;
+        case 'C':
+            System.out.println("value is "+switchValue);
+            break;
+            case 'D':
+            System.out.println("value is "+switchValue);
+            break;
+        default:
+            System.out.println("value is "+switchValue);
+            break;
+    }
+    }
+}
